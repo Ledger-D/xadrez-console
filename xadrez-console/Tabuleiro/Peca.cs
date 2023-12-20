ecd5c00f9a9fbae15a198081ed758628cd7c40a1
Ledger-D <danielnoaves2011@gmail.com>
2023-12-19 22:32:09 -0300

    meu primeiro commit

---
+++ xadrez-console/Tabuleiro/Peca.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimento { get; protected set; }
+        public Tabuleiro tabuleiro { get; protected set; }
+
+    }
+}
