45615102caa5fed15950b1ae1a691da84a396c02
Ledger-D <danielnoaves2011@gmail.com>
2023-12-28 23:10:41 -0300

    começando a colocar peças no tabuleiro

--- xadrez-console/Tabuleiro/Peca.cs
+++ xadrez-console/Tabuleiro/Peca.cs
@@ -9,12 +9,17 @@ namespace tabuleiro
         public int qteMovimento { get; protected set; }
         public Tabuleiro tabuleiro { get; protected set; }
 
-        public Peca (Posicao posicao,   Tabuleiro tabuleiro, Cor cor)
+
+
+
+            public Peca(Posicao posicao, Tabuleiro tabuleiro, Cor cor)
         {
             this.posicao = posicao;
             this.cor = cor;
             this.tabuleiro = tabuleiro;
             this.qteMovimento = 0;
         }
+       
+
     }
 }
--- xadrez-console/Tabuleiro/Tabuleiro.cs
+++ xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -11,12 +11,26 @@ namespace tabuleiro
         {
             this.linhas = linhas;
             this.colunas = colunas;
-            pecas =new Peca[linhas,colunas];
+            pecas = new Peca[linhas, colunas];
 
 
         }
+        public Peca peca(int linha, int coluna) {
 
+            return pecas[linha, coluna];
+        
+        }
 
+      
 
     }
+
+
+
 }
+
+
+
+
+
+
