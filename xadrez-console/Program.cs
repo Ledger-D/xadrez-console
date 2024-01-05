6b54f4f186bd73a22dc2a9c5a7b8261a9598f900
Ledger-D <danielnoaves2011@gmail.com>
2024-01-04 23:07:10 -0300

    criando a class posição xadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -7,28 +7,39 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(9, 7));
-                tab.colocarPeca(new Torre (tab, Cor.Preta), new Posicao(2, 7));
+            Console.WriteLine('a' - 0 );
+           
 
 
-                Cor c = Cor.Branca;
-                Tela.imprimirTabuleiro(tab);
 
 
-            }
-            catch(TabuleiroException e) {
-                Console.WriteLine("Erro: "+ e.Message );
+            /*
+        Console.WriteLine("============================\r\n++++++++++++++++++++++++++++\r\n\\  /        |\\\t|\\   __  ___\r\n \\/    /\\   | \\\t| \\ |\t   /\r\n /\\   /--\\  | / | / |==\t  /\r\n/  \\ / \t  \\ |/  | \\ |__\t /___\r\n+++++++++++++++++++++++++++++\r\n=============================");
+        try
+        {
 
-                
-            }
- 
- 
- 
- }
-   
+
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 7));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(2, 7));
+
+
+            Cor c = Cor.Branca;
+            Tela.imprimirTabuleiro(tab);
+
+
+        }
+        catch (TabuleiroException e)
+        {
+            Console.WriteLine("Erro: " + e.Message);
+
+
+        }
+
+            */
+
+        }
+
     }
 }
